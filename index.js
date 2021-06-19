53961c496c5b696243f29bcfbf0b9d0775c0d9e8
Sebastián M. Pérez <perezsebastianm@gmail.com>
2021-06-19 01:14:12 -0300

    Fix linter errors

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/* eslint-disable import/no-dynamic-require */
+/* eslint-disable global-require */
 const fs = require('fs');
 const qrcode = require('qrcode-terminal');
 const { Client } = require('whatsapp-web.js');
@@ -5,25 +7,25 @@ const { SESSION_FILE_PATH } = require('./lib/config');
 const { handleMessage } = require('./lib/commands');
 
 let sessionData;
-if(fs.existsSync(SESSION_FILE_PATH)) {
-    sessionData = require(SESSION_FILE_PATH);
+if (fs.existsSync(SESSION_FILE_PATH)) {
+  sessionData = require(SESSION_FILE_PATH);
 }
 
 const client = new Client({
-    session: sessionData
+  session: sessionData,
 });
 
 client.on('authenticated', (session) => {
-    sessionData = session;
-    fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function (err) {
-        if (err) {
-            console.error(err);
-        }
-    });
+  sessionData = session;
+  fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
 });
 
-client.on('qr', qr => {
-  qrcode.generate(qr, {small: true});
+client.on('qr', (qr) => {
+  qrcode.generate(qr, { small: true });
 });
 
 client.on('ready', () => {
